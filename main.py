31cbafe2322f6124b6003d9a74faf635f68623b6
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-08-14 16:20:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-  question = input("Que puis-je faire pour vous ?")
-  
+##### SETTINGS #####
+settings = {"language": "FR-fr"}
 
-if __name__ == '__main__':
-  main()
+##### CODE #####
+question = input("Que puis-je faire pour vous ?")
+
+def main(lang):
+  questionsplit = question.split()
+  if lang == "FR-fr":
+    if questionsplit[0] == "ping":
+      url = questionsplit[1]
+      status_website = urllib.request.urlopen(url).getcode()
+      website_is_up = status_website == 200
+      print(website_is_up)
+      question = input("Que puis-je faire pour vous ?")
+    if questionsplit[0] == "Crée" and questionsplit[1] == "raccourci":
+      nameshortcut = input("Quel nom voulez vous donnez à votre raccourci ?")
+      websiteshortcut = input("Quelle est l'adresse du site de votre raccourci ?")
+      settings[nameshortcut] = websiteshortcut 
+      question = input("Que puis-je faire pour vous ?")
+      
+      
+if '__name__' == '__main__':
+    main(settings["language"])
