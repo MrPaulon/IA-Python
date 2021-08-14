472920aa0f8c326a64da310b4b6dfd07bfe5601d
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-08-14 16:46:22 +0200

    Update main.py
    Ajout de la création de qrcode

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 ##### SETTINGS #####
 settings = {"language": "FR-fr"}
 
+##### APPS #####
+import qrcode
 ##### CODE #####
 question = input("Que puis-je faire pour vous ?")
 
@@ -14,11 +16,18 @@ def main(lang):
       print(website_is_up)
       question = input("Que puis-je faire pour vous ?")
     if questionsplit[0] == "Crée" and questionsplit[1] == "raccourci":
-      nameshortcut = input("Quel nom voulez vous donnez à votre raccourci ?")
-      websiteshortcut = input("Quelle est l'adresse du site de votre raccourci ?")
-      settings[nameshortcut] = websiteshortcut 
+      name_shortcut = input("Quel nom voulez vous donnez à votre raccourci ?")
+      website_shortcut = input("Quelle est l'adresse du site de votre raccourci ?")
+      settings[name_shortcut] = website_shortcut 
       question = input("Que puis-je faire pour vous ?")
-      
+    if questionsplit[0] == "Crée" and questionsplit[1] == "qrcode":
+      qrcode_custom = input("Souhaitez-vous que je crée un qrcode simple ? Ou préférez vous le personaliser ?")
+      if qrcode_custom == "simple":
+        qrcode_website = input("Quel est le site internet relié au qrcode ?")
+        qrcode_img = qrcode.make(qrcode_website)
+        qrcode_img.save('qrcode.png')
+      if qrcode_custom == "custom":
+        
       
 if '__name__' == '__main__':
     main(settings["language"])
