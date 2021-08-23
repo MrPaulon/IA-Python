974308ed453fff5f44a9e7a51eda79af054ae21d
Paul <76972320+MrPaulon@users.noreply.github.com>
2021-08-23 12:56:58 +0200

    [+] Etat réseaux

--- src/main.py
+++ src/main.py
@@ -6,29 +6,29 @@ import qrcode
 from qrcode import *
 ##### CODE #####
 
-print(settings["language"])
+lang = settings["language"]
 
-def main(lang):
-  if lang == 'FR-fr':
+if lang == 'FR-fr':
+  question = input("Que puis-je faire pour vous ?")
+  questionsplit = question.split()
+  if questionsplit[0] == "ping":
+    ping_website = questionsplit[0] + " " + questionsplit[1]
+    os.system(ping_website)
+  if questionsplit[0] == "Crée" and questionsplit[1] == "raccourci":
+    name_shortcut = input("Quel nom voulez vous donnez à votre raccourci ?")
+    website_shortcut = input("Quelle est l'adresse du site de votre raccourci ?")
+    settings[name_shortcut] = website_shortcut 
     question = input("Que puis-je faire pour vous ?")
-    questionsplit = question.split()
-    if questionsplit[0] == "ping":
-      ping_website = questionsplit[0] + " " + questionsplit[1]
-      os.system(ping_website)
-      return
-    if questionsplit[0] == "Crée" and questionsplit[1] == "raccourci":
-      name_shortcut = input("Quel nom voulez vous donnez à votre raccourci ?")
-      website_shortcut = input("Quelle est l'adresse du site de votre raccourci ?")
-      settings[name_shortcut] = website_shortcut 
-      question = input("Que puis-je faire pour vous ?")
-    if questionsplit[0] == "Crée" and questionsplit[1] == "qrcode":
-      qrcode_custom = input("Souhaitez-vous que je crée un qrcode simple ? Ou préférez vous le personaliser ?")
-      if qrcode_custom == "simple":
-        qrcode_website = input("Quel est le site internet relié au qrcode ?")
-        qrcode_img = qrcode.make(qrcode_website)
-        qrcode_img.save('qrcode.png')
-      if qrcode_custom == "custom":
-        qrcode_website = input("Quel est le site internet relié au qrcode ?")
-        
+  if questionsplit[0] == "Crée" and questionsplit[1] == "qrcode":
+    qrcode_custom = input("Souhaitez-vous que je crée un qrcode simple ? Ou préférez vous le personaliser ?")
+    if qrcode_custom == "simple":
+      qrcode_website = input("Quel est le site internet relié au qrcode ?")
+      qrcode_img = qrcode.make(qrcode_website)
+      qrcode_file = input("Quel nom voulez vous donnez à votre fichier ?")
+      qrcode_img.save(qrcode_file + '.png')
+    if qrcode_custom == "custom":
+      qrcode_website = input("Quel est le site internet relié au qrcode ?")
+  if questionsplit[0] == "Etat" and questionsplit[1] == "réseaux":
+    os.system("ipconfig")
       
 main(settings["language"])
