33c194409a7d7321c77b0f6fdca2bc5b7f561101
Paul <76972320+MrPaulon@users.noreply.github.com>
2022-04-13 13:56:41 +0200

    Prise en charge de plusieurs languages (FR + EN)

--- src/main.py
+++ src/main.py
@@ -9,26 +9,21 @@ from qrcode import *
 print(settings["language"])
 
 def main(lang):
-  if lang == 'FR-fr':
-    question = input("Que puis-je faire pour vous ?")
+    question = input(language[lang]['Hello what i can do ?']).lower()
     questionsplit = question.split()
-    if questionsplit[0] == "ping":
-      ping_website = questionsplit[0] + " " + questionsplit[1]
-      os.system(ping_website)
-      return
-    if questionsplit[0] == "Crée" and questionsplit[1] == "raccourci":
-      name_shortcut = input("Quel nom voulez vous donnez à votre raccourci ?")
-      website_shortcut = input("Quelle est l'adresse du site de votre raccourci ?")
-      settings[name_shortcut] = website_shortcut 
-      question = input("Que puis-je faire pour vous ?")
-    if questionsplit[0] == "Crée" and questionsplit[1] == "qrcode":
-      qrcode_custom = input("Souhaitez-vous que je crée un qrcode simple ? Ou préférez vous le personaliser ?")
-      if qrcode_custom == "simple":
-        qrcode_website = input("Quel est le site internet relié au qrcode ?")
-        qrcode_img = qrcode.make(qrcode_website)
-        qrcode_img.save('qrcode.png')
-      if qrcode_custom == "custom":
-        qrcode_website = input("Quel est le site internet relié au qrcode ?")
-        
-      
+    for i in range(len(questionsplit)):
+        if questionsplit[i] == "ping":
+          ping_website = questionsplit[i] + " " + questionsplit[i+1]
+          os.system(ping_website)
+          return
+        if questionsplit[i] == language[lang]['create'] and questionsplit[i+1] == "qrcode":
+          qrcode_custom = input(language[lang]['qrcode_creation'])
+          if qrcode_custom == "simple":
+            qrcode_website = input(language[lang]['qrcode_namewebsite'])
+            qrcode_img = qrcode.make(qrcode_website)
+            qrcode_img.save('qrcode.png')
+          if qrcode_custom == "custom":
+            qrcode_website = input("Quel est le site internet relié au qrcode ?")
+
+
 main(settings["language"])
--- src/settings.py
+++ src/settings.py
@@ -2,3 +2,18 @@
 settings = {
 	"language": "FR-fr"
 }
+
+language = {
+	"FR-fr" : {
+		'Hello what i can do ?' : 'Bonjour que puis-je faire pour vous ?',
+		'hello' : 'bonjour',
+		'create': 'crée',
+		'qrcode_creation' : 'Souhaitez-vous que je crée un qrcode simple ? Ou préférez vous le personaliser ?',
+		'qrcode_namewebsite' : 'Quel est le site internet relié au qrcode ?'
+	},
+	"EN-en" : {
+		'Hello what i can do ?' : 'Hello what can I do for you?',
+		'hello' : 'hello',
+		'create': 'create'
+	}
+}
